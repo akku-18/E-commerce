7d2cc35c66abd805d572c42a4e56485a7ccd8eca
akku-18 <akshanshgarg18@gmail.com>
2023-04-11 22:57:09 +0530

    designed banner section

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,27 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>Sales</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi culpa
+            labore cum voluptates earum quidem, error ipsam ea, reprehenderit
+            vitae vero similique ipsa excepturi vel iste magnam. Quaerat,
+            eligendi eum!
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img src={BannerImg} alt="" className="banner-img"/>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
