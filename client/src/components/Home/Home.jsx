ebb174d73ac5ebca5537da33a6dc8160f763687b
akku-18 <akshanshgarg18@gmail.com>
2023-04-22 10:28:35 +0530

    build api

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,29 @@
+import { useEffect } from "react";
 import "./Home.scss";
+
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromAPI } from "../../utils/api";
 
 const Home = () => {
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = () => {
+    fetchDataFromAPI("/api/categories?populate=*").then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
-        <Category />
-        <Products headingText="Popular Products" />
+          <Category />
+          <Products headingText="Popular Products" />
         </div>
       </div>
     </div>
