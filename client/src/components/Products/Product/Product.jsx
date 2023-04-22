3edf37c15d0a70dad7a25dfee6af7b6bde77e75c
akku-18 <akshanshgarg18@gmail.com>
2023-04-22 14:35:10 +0530

    fixed errors

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,7 +1,10 @@
 import "./Product.scss";
+import {useNavigate} from 'react-router-dom';
 
 const Product = ({id, data}) => {
-    return <div className="product-card">
+    const navigate = useNavigate()
+
+    return <div className="product-card" onClick={()=> navigate("/product/"+id)}>
         <div className="thumbnail">
             <img src={process.env.REACT_APP_DEV_URL + data.img.data[0].attributes.url} alt="" />
         </div>
--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -1,4 +1,9 @@
 import "./SingleProduct.scss";
+
+import { useContext, useState } from "react";
+import { Context } from "../../utils/context";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 import RelatedProducts from "./RelatedProducts/RelatedProducts";
 
 import {
@@ -12,22 +17,46 @@ import {
 import prod from "../../assets/products/speaker-prod-3.webp";
 
 const SingleProduct = () => {
+  const [quantity, setQuantity] = useState(1);
+  const { id } = useParams();
+  const { handleAddToCart } = useContext(Context);
+  const { data } = useFetch(`/api/products?populate=*&[filters][id]=${id}`);
+
+  const decrement = () => {
+    setQuantity((prevState) => {
+      if (prevState === 1) return 1;
+      return prevState - 1;
+    });
+  };
+  const increment = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  if (!data) return;
+  const product = data?.data?.[0]?.attributes;
+
   return (
     <div className="single-product-main-content">
       <div className="layout">
         <div className="single-product-page">
           <div className="left">
-            <img src={prod} alt="" />
+            <img
+              src={
+                process.env.REACT_APP_DEV_URL +
+                product.img.data[0].attributes.url
+              }
+              alt=""
+            />
           </div>
           <div className="right">
-            <span className="name">Name</span>
-            <span className="price">Price</span>
-            <span className="desc">Description</span>
+            <span className="name">{product.title}</span>
+            <span className="price">&#8377;{product.price}</span>
+            <span className="desc">{product.description}</span>
             <div className="cart-buttons">
               <div className="quantity-buttons">
-                <span>-</span>
-                <span>2</span>
-                <span>+</span>
+                <span onClick={decrement}>-</span>
+                <span>{quantity}</span>
+                <span onClick={increment}>+</span>
               </div>
               <button className="add-to-cart-button">
                 <FaCartPlus size={20} /> Add to cart
